1a5d9859f7da058ff497c28861e5fbf6026b22aa
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-02-02 19:01:50 +0000

    started work on login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Login from "./pages/Login/Login"
 import SignUp from "./pages/SignUp/SignUp"
 import Home from "./pages/Home/Home"
-
+import './App.scss'
 // Main App component, holds the routes that lead to other pages
 function App() {
 
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,8 +1,14 @@
+import './Login.scss'
+
 export default function Login(){
     return (
-        <>
-            <h1>login page</h1>
-            <a href="/signup">sign up page</a>
-        </>
+        <div className='content-container'>
+           <h1 className='header'>Login page</h1>
+           <div className='login-container'>
+               <input className='input-box' type="email" placeholder='Enter your email'/>
+               <input className='input-box' type="password" placeholder='Enter your password'/>
+               <button className='login-button'>Log in</button>
+           </div>
+        </div>
     )
 }
