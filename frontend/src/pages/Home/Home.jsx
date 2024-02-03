282c7d1a60fe3f31cc8843339aa6e8375a2448a0
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-02-03 19:00:18 +0000

    added comments to the home component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
-// The landing page component
+/* The landing page component - if you found this, congrats!
+The markup/html goes into the return statement below
+Your css goes into the Home.scss file, and you can navigate to your page by going to localhost:5173/
+*/
 
-export default function Home(){
-    return (
-        <>
-            <h1>Home page page</h1>
-            <a href="/login">login page</a>
-        </>
-    )
+export default function Home() {
+  return (
+    <>
+      <h1>Home page page</h1>
+      <a href="/login">login page</a>
+    </>
+  );
 }
