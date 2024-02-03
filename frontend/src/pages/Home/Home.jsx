662cd356421bcec92b217e58d6ba59318b76a7c6
q04784mt <mara-ecaterina.tarhon@student.manchester.ac.uk>
2024-02-03 23:41:48 +0000

    unfinished first draft

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,12 +2,23 @@
 The markup/html goes into the return statement below
 Your css goes into the Home.scss file, and you can navigate to your page by going to localhost:5173/
 */
-
+import './Home.scss'
 export default function Home() {
   return (
-    <>
-      <h1>Home page page</h1>
-      <a href="/login">login page</a>
-    </>
+
+    <div className='main-container'>
+      <h1 className='header' >Year2Year</h1>
+      <div className='body'>
+     
+        <button className='button'>Sign in:)</button>
+        <br/>
+        <p className='p1'>Smth smth, i dont understand why this thing is here on the right</p>
+        <div class="footer">
+          three buttons go here, profilr/homr/book now
+        </div>
+
+      </div>
+     
+    </div>
   );
 }
