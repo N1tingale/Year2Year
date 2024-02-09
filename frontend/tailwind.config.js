39bdb19c16da8f78d74b0a129ca810d0401340a7
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-02-09 17:26:38 +0000

    custom color theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,29 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0c2d1c",
+
+          secondary: "#0c2d1c",
+
+          accent: "#0c2d1c",
+
+          neutral: "#0c2d1c",
+
+          "base-100": "#e8c497",
+
+          info: "#0c2d1c",
+
+          success: "#00ffff",
+
+          warning: "#0c2d1c",
+
+          error: "#0c2d1c",
+        },
+      },
+    ],
+  },
 };
