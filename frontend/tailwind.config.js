aa5a7df8b4d1c3db138455687a1b7784efb15c87
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-02-10 21:40:29 +0000

    imported the lexend font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
@@ -6,6 +8,9 @@ module.exports = {
       colors: {
         primaryColor: "#0c2d1c",
         "primaryColor-800": "#154732",
+      },
+      fontFamily: {
+        sans: ['"Lexend"', ...defaultTheme.fontFamily.sans],
       },
     },
   },
