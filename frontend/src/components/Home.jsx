471adcc52625bb33bce1fe395fc632dc5d92b90a
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-02-10 21:40:20 +0000

    touched up the colours

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="h-screen">
       <Navbar />
       <div className="flex flex-col items-center">
-        <h1 className="text-5xl font-bold text-center mt-4">
+        <h1 className="text-5xl font-bold text-primaryColor text-center mt-4">
           UNLOCK YOUR POTENTIAL: BOOK YOUR TUTOR, <br /> MASTER YOUR SUBJECTS!
         </h1>
         <p className="text-xl mt-6">How It Works</p>
@@ -19,7 +19,7 @@ export default function Home() {
       <div className="flex justify-center items-center mt-4">
         <div className="card bg-primaryColor text-white mx-4 shadow-xl outline-primary">
           <div className="card-body p-6">
-            <p className="text-2xl font-semibold whitespace-nowrap">
+            <p className="text-2xl whitespace-nowrap">
               <p className="font-bold">SIGN UP USING</p>
               YOUR UNIVERSITY EMAIL
             </p>
@@ -27,7 +27,7 @@ export default function Home() {
         </div>
         <div className="bg-white card mx-4 shadow-xl outline-primary">
           <div className="card-body p-6">
-            <p className="text-2xl font-semibold whitespace-nowrap">
+            <p className="text-2xl whitespace-nowrap">
               <p className="font-bold">PICK THE MODULES</p>
               YOU NEED HELP WITH
             </p>
@@ -35,7 +35,7 @@ export default function Home() {
         </div>
         <div className="card mx-4 bg-primaryColor text-white shadow-xl outline-primary">
           <div className="card-body p-6">
-            <p className="text-2xl font-semibold whitespace-nowrap">
+            <p className="text-2xl whitespace-nowrap">
               <p className="font-bold">SCHEDULE A MEETING</p>
               WITH YOUR NEW TUTOR
             </p>
@@ -43,13 +43,13 @@ export default function Home() {
         </div>
       </div>
       <div className="flex flex-col justify-center items-center mt-8">
-        <p className="text-4xl">Still Waiting?</p>
+        <p className="text-4xl font-light">Still Waiting?</p>
         <h1 className="text-4xl font-bold mt-1">
           Embark On Your Learning Journey With Us!
         </h1>
         <p className="text-xl mt-1">Don't Miss Out on Success:</p>
         <p className="text-xl font-bold">Book Your Tutor Now!</p>
-        <button className="shadow-xl btn btn-primary mt-4 rounded-3xl pr-0">
+        <button className="shadow-xl text-white btn btn-primary mt-4 rounded-3xl pr-0">
           Sign Up
           <button className="btn btn-circle bg-white outline outline-primaryColor hover:bg-white">
             <FaArrowRightLong className="h-5 w-5" />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
-    <div className="flex justify-center">
+    <div className="flex text-primaryColor justify-center">
       <div className="navbar bg-base-100 justify-evenly w-10/12 border-b border-primary">
         <Link className="text-xl font-semibold ml-52" to="/">
           Home
