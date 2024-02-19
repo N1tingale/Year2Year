2e6ac8c3e0af8ee447854181537452cf8e4f4776
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-02-19 21:50:19 +0000

    installed axios + worked on login page state

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
 import Navbar from "./Navbar";
+import axios from 'axios';
 export default function Login() {
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  const sendLogInData = () =>  {
+    axios.post('http://127.0.0.1:5000/login', {
+      "email":email,
+      "password":password
+    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className="overflow-hidden h-screen">
       <Navbar />
@@ -13,6 +28,7 @@ export default function Login() {
             type="email"
             placeholder="Email"
             className="input input-bordered w-full max-w-xs placeholder:text-primaryColor-800"
+            onChange={(e) => setEmail(e.target.value)}
           />
         </label>
         <label className="form-control w-full max-w-xs">
@@ -23,9 +39,10 @@ export default function Login() {
             type="password"
             placeholder="Password"
             className="input input-bordered w-full max-w-xs placeholder:text-primaryColor-800"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </label>
-        <button className="btn btn-primary mt-4">Log in</button>
+        <button className="btn btn-primary mt-4"  onClick={sendLogInData}>Log in</button>
       </div>
     </div>
   );
