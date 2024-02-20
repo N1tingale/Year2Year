cd66cf6b4d2b78c6ac6986ba050fc2bcf88b8bf3
Daniel Merrett <daniel.merrett04@gmail.com>
2024-02-20 13:40:00 +0000

    changes to routes for hashing

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,11 +2,13 @@ from app import app, db
 from app.models import Student, Tutor, Module, Booking, Report
 from flask import jsonify, request
 from sqlalchemy import select
-
+from app.hash_pass import hash_data
 
 # API Routes are instantiated here
 # Each decorator such as @app.route defines an endpoint (such as /students) and a method for that endpoint
 # The function below the decorator handles the data sent/received by the endpoint
+
+
 @app.route('/students', methods=['GET'])
 def get_students():
     students = Student.query.all()
@@ -14,6 +16,7 @@ def get_students():
                                   'first_name': student.first_name,
                                   'last_name': student.last_name,
                                   'email': student.email} for student in students]})
+
 
 @app.route('/add-student', methods=['POST'])
 def create_student():
@@ -25,12 +28,14 @@ def create_student():
     password = data.get('password')
     emailVerified = False
 
+    hashed_password = hash_data(password)
 
     try:
         if not all([first_name, last_name, email, password]):
             return jsonify({'error': 'All fields (first_name, last_name, email, password) are required'}), 400
 
-        new_student = Student(first_name=first_name, last_name=last_name, email=email, password=password, emailVerified=emailVerified)
+        new_student = Student(first_name=first_name, last_name=last_name,
+                              email=email, password=hashed_password, emailVerified=emailVerified)
 
         db.session.add(new_student)
         db.session.commit()
@@ -44,21 +49,25 @@ def create_student():
     except:
         return jsonify({'error': "Student not created"}), 400
 
+
 @app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
 
     email = data.get("email")
     password = data.get("password")
+    hashed_password = hash_data(password)
 
     student = None
     tutor = None
 
     try:
-        student = Student.query.filter_by(email=email, password=password).first()
+        student = Student.query.filter_by(
+            email=email, password=hashed_password).first()
         if student:
             return jsonify({"message": "This is a student successfully logging in"}), 201
-        tutor = Tutor.query.filter_by(email=email, password=password).first()
+        tutor = Tutor.query.filter_by(
+            email=email, password=hashed_password).first()
         if tutor:
             return jsonify({"message": "This is a tutor successfully logging in"}), 201
         return jsonify({"error": "Unsuccessful login"}), 400
@@ -79,9 +88,10 @@ def get_tutors():
 def get_tutor_id(tutorId):
     tutor = Tutor.query.get(tutorId)
     return jsonify({'tutor': {'id': tutor.id,
-                                'first_name': tutor.first_name,
-                                'last_name': tutor.last_name,
-                                'email': tutor.email}})
+                              'first_name': tutor.first_name,
+                              'last_name': tutor.last_name,
+                              'email': tutor.email}})
+
 
 @app.route('/tutors', methods=['POST'])
 def create_tutor():
@@ -91,6 +101,7 @@ def create_tutor():
     last_name = data.get('last_name')
     email = data.get('email')
     password = data.get('password')
+    hashed_password = hash_data(password)
     year = data.get('year')
     contact_number = data.get('contact_number')
     description = data.get('description')
@@ -101,21 +112,22 @@ def create_tutor():
         if not all([first_name, last_name, email, password]):
             return jsonify({'error': 'All fields (first_name, last_name, email, password) are required'}), 400
 
-        new_tutor = Tutor(first_name=first_name, last_name=last_name, email=email, password=password, year=year, \
-                        contact_number=contact_number, description= description, emailVerified=emailVerified)
+        new_tutor = Tutor(first_name=first_name, last_name=last_name, email=email, password=hashed_password, year=year,
+                          contact_number=contact_number, description=description, emailVerified=emailVerified)
 
         db.session.add(new_tutor)
         db.session.commit()
 
         return jsonify({'message': 'Tutor created successfully',
                         'tutor': {'id': new_tutor.id,
-                                    'first_name': new_tutor.first_name,
-                                    'last_name': new_tutor.last_name,
-                                    'email': new_tutor.email,
-                                    "emailVerified": new_tutor.emailVerified}}), 201
+                                  'first_name': new_tutor.first_name,
+                                  'last_name': new_tutor.last_name,
+                                  'email': new_tutor.email,
+                                  "emailVerified": new_tutor.emailVerified}}), 201
 
     except:
         return jsonify({"error": "Tutor not created"}), 400
+
 
 @app.route('/modules', methods=['GET'])
 def get_modules():
@@ -125,16 +137,18 @@ def get_modules():
                                  'module_name': module.module_name,
                                  'tutor_id': module.tutor_id}for module in modules]})
 
+
 @app.route('/bookings', methods=['GET'])
 def get_bookings():
     bookings = Booking.query.all()
     return jsonify({'bookings': [{'id': booking.id,
-                                'student_id': booking.student_id,
-                                'tutor_id': booking.tutor_id,
-                                'module_id': booking.module_id,
-                                'time': booking.time,
-                                'location': booking.location,
-                                'description': booking.description}for booking in bookings]})
+                                  'student_id': booking.student_id,
+                                  'tutor_id': booking.tutor_id,
+                                  'module_id': booking.module_id,
+                                  'time': booking.time,
+                                  'location': booking.location,
+                                  'description': booking.description}for booking in bookings]})
+
 
 @app.route('/bookings', methods=['POST'])
 def create_bookings():
@@ -150,9 +164,11 @@ def create_bookings():
     if not all([student_id, tutor_id, module_id, time, location, description]):
         return jsonify({'error': 'All fields (student_id, tutor_id, module_id, time, location, description) are required'}), 400
 
-    new_booking = Booking(student_id=student_id, tutor_id=tutor_id, module_id=module_id, time=time, location=location, description=description)
+    new_booking = Booking(student_id=student_id, tutor_id=tutor_id,
+                          module_id=module_id, time=time, location=location, description=description)
     db.session.add(new_booking)
     db.session.commit()
+
 
 @app.route('/reports', methods=['GET'])
 def get_reports():
@@ -163,5 +179,3 @@ def get_reports():
                                  'module_id': report.module_id,
                                  'type': report.type,
                                  'description': report.description}for report in reports]})
-
-
