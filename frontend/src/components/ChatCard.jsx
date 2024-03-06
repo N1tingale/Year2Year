f76b23ad0c166b2bf53aceb81acc5870fd321106
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-03-06 16:11:13 +0000

    improved chat card design

--- frontend/src/components/ChatCard.jsx
+++ frontend/src/components/ChatCard.jsx
@@ -1,8 +1,13 @@
+import { RxAvatar } from "react-icons/rx";
+
 export default function ChatCard({ tutorName, message }) {
   return (
-    <div className="bg-white p-4 m-2 rounded-3xl border-2 border-black">
-      <h1 className="text-xl font-bold">{tutorName}</h1>
-      <p>{message}</p>
+    <div className="bg-white p-4 m-2 rounded-3xl border-2 flex items-center border-black">
+      <RxAvatar className="w-12 h-12 mx-1" />
+      <div>
+        <h1 className="text-xl font-bold">{tutorName}</h1>
+        <p>{message}</p>
+      </div>
     </div>
   );
 }
