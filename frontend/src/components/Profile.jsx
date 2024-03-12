5371ad8fadf8c6fb511dec186512e50afe89afcd
q04784mt <mara-ecaterina.tarhon@student.manchester.ac.uk>
2024-03-12 01:38:53 +0000

    name+bio

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -21,9 +21,27 @@ export default function Profile() {
     <>
       <Navbar />
       {authenticated ? (
-        <div className="w-2/3 mx-auto mt-2 rounded-xl flex justify-between">
-          <div className="rounded-xl shadow-2xl flex items-center justify-center">
-            Profile goes here
+        <div className="w-2/3 mx-auto mt-2 rounded-xl flex justify-between divider-vertical">
+          <div className="rounded-xl shadow-2xl flex flex-col w-5/12 p-4">
+
+
+            <div className="flex justify-center items-center rounded-xl border-2 border-black m-2 bg-white w-1/3">
+              <h1 className="text-3xl font-extrabold p-4">Profile</h1>
+            </div>
+
+              <div className="flex flex-col items-center rounded-xl border-2 border-black m-2 bg-white w-1/3s p-2">
+                <h1 className="text-xl font-bold p-2">Your name</h1>
+                
+                <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+               
+              </div>
+              
+              <div className="flex flex-col items-center rounded-xl border-2 border-black m-2 bg-white w-1/3s p-2">
+                <h1 className="text-xl font-bold p-2">About</h1>
+                <textarea class="textarea textarea-bordered w-full" placeholder="Bio"></textarea>
+              </div>
+
+
           </div>
           <div className="rounded-xl shadow-2xl flex flex-col w-5/12 p-4">
             <div className="flex justify-center items-center rounded-xl border-2 border-black m-2 bg-white w-1/3">
