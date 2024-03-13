268f681688e1dd6770ddfc95870d56d0a1913d56
Insiya Mullamitha <insiya.mullamitha@gmail.com>
2024-03-13 14:30:41 +0000

    Remove run_socketio.py file

--- backend/run_socketio.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_socketio import SocketIO
-from app import app
-
-socketio = SocketIO(app, cors_allowed_origins="*")
-
-if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5001)
