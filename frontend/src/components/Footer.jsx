9851fa1687198d5fe9235eaa5ac60a056706ed06
Insiya Mullamitha <insiya.mullamitha@gmail.com>
2024-03-23 22:17:19 +0000

    Contact us mailto in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,10 @@
-export default function Footer({relative}) {
+export default function Footer({ relative }) {
   return (
-    <footer className={`${relative ? "relative" : "absolute left-0 bottom-0"} footer items-center px-2 py-1 bg-primaryColor`}>
+    <footer
+      className={`${
+        relative ? "relative" : "absolute left-0 bottom-0"
+      } footer items-center px-2 py-1 bg-primaryColor`}
+    >
       <aside className="text-white items-center grid-flow-col">
         <svg
           width="36"
@@ -16,8 +20,8 @@ export default function Footer({relative}) {
         <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
       </aside>
       <nav className="grid-flow-col gap-4 text-white flex md:place-self-center md:justify-self-end">
-        <div className="flex gap-2 text-lg font-semibold text-right">
-          <p>Contact Us</p>
+        <div className="flex gap-4 text-lg font-semibold text-right">
+          <a href="mailto:year2yearmcr@gmail.com">Contact Us</a>
           <p>Privacy Policy</p>
         </div>
       </nav>
