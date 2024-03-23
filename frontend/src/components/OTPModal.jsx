bd9c546ff867756e8dcdbf331d3383a8ae35e16b
Insiya Mullamitha <insiya.mullamitha@gmail.com>
2024-03-23 21:52:09 +0000

    improve otp modal styling

--- frontend/src/components/OTPModal.jsx
+++ frontend/src/components/OTPModal.jsx
@@ -56,32 +56,34 @@ export default function OTPModal({
       {showModal && (
         <div
           id="otp_modal"
-          className="fixed inset-0 z-50 flex items-center justify-center"
+          className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-20"
         >
-          <div className="modal-box text-center bg-base rounded-xl shadow-xl p-8">
-            <h3 className="font-bold text-lg">Enter your one-time password</h3>
-            <p className="py-4">Sent to {email}</p>
+          <div className="modal-box text-center bg-base rounded-lg shadow-xl p-8">
+            <h3 className="font-bold text-2xl mb-4">
+              Enter your one-time password
+            </h3>
+            <p className="text-lg mb-4">Sent to {email}</p>
             <MuiOtpInput
               sx={{
                 input: {
                   border: "none",
                 },
               }}
-              className="w-4/6 flex mx-auto justify-center"
+              className="w-4/6 mx-auto text-xl"
               value={otp}
               onChange={handleChange}
               validateChar={validateChar}
             />
-            <div className="flex justify-center gap-4">
+            <div className="flex justify-center mt-4">
               {!correctOtp && (
                 <Link to="/login">
-                  <button className="btn bg-primaryColor text-white mt-4">
+                  <button className="btn bg-white text-gray-800 mr-4">
                     Go Back
                   </button>
                 </Link>
               )}
               <button
-                className="btn bg-primaryColor text-white mt-4"
+                className="btn bg-primaryColor text-white"
                 onClick={() => resendOTP(email)}
               >
                 Resend OTP
