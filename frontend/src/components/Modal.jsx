df89a7f115157dcffd198199dd28e43ddef2e8a5
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-03-30 15:02:52 +0000

    added date formatting

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -14,6 +14,34 @@ export default function Modal({
   recipientProfileColour,
   chId,
 }) {
+  function formatDate(timestamp) {
+    const date = new Date(timestamp);
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(today.getDate() - 1);
+
+    const oneWeekAgo = new Date(today);
+    oneWeekAgo.setDate(today.getDate() - 7);
+
+    let day;
+    if (date.toDateString() === today.toDateString()) {
+      day = "";
+    } else if (date.toDateString() === yesterday.toDateString()) {
+      day = "Yesterday, ";
+    } else if (date.getTime() >= oneWeekAgo.getTime()) {
+      day = date.toLocaleDateString("en-US", { weekday: "long" }) + ", ";
+    } else {
+      day = date.toLocaleDateString() + ", ";
+    }
+
+    const time = date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    return `${day}${time}`;
+  }
+
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const userId = localStorage.getItem("id");
@@ -188,10 +216,7 @@ export default function Modal({
                 </div>
                 <div className="chat-header">
                   <time className="text-xs opacity-50 mx-1">
-                    {new Date(msg.timestamp).toLocaleTimeString([], {
-                      hour: "2-digit",
-                      minute: "2-digit",
-                    })}
+                    {formatDate(msg.timestamp)}
                   </time>
                 </div>
                 <div
