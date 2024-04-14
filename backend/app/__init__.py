eea237e91ae3e73a4aa8662909a41b89b75bb1f2
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-04-14 22:15:05 +0100

    conversion from wsgi to asgi

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,11 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_socketio import SocketIO
 from flask_mail import Mail, Message
+from asgiref.wsgi import WsgiToAsgi
+
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 app.config.from_object('config.Config')
 CORS(app, resources={r"/*": {"origins": "*"}})
+
+asgi_app = WsgiToAsgi(app)
 
 mail = Mail(app)
 db = SQLAlchemy(app)
