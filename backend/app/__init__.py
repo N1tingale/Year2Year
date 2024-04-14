f23be8a652f4729cd983d0b42bc63cea87bd0c85
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-04-14 23:54:34 +0100

    Revert "production change 2 electric boogaloo"
    This reverts commit f6e9974e572dd4f951e9aff12c42470ef8f25b3f.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_socketio import SocketIO
 from flask_mail import Mail, Message
-from asgiref.wsgi import WsgiToAsgi
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 app.config.from_object('config.Config')
 CORS(app, resources={r"/*": {"origins": "*"}})
-
-asgi_app = WsgiToAsgi(app)
 
 mail = Mail(app)
 db = SQLAlchemy(app)
