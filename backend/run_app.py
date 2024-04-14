752cd2ffd6708ef6ba12b1b856cd8683e90f6b26
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-04-14 19:53:58 +0100

    prod commit

--- backend/run_app.py
+++ backend/run_app.py
@@ -7,5 +7,5 @@ if not os.path.exists("backend/instance/site.db"):
     with app.app_context():
         db.create_all()
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
