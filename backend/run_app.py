35e300f33e664c5f7d78b3eb88c6c78ef876ecbc
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-04-14 21:55:39 +0100

    change prod run code

--- backend/run_app.py
+++ backend/run_app.py
@@ -1,5 +1,4 @@
 from app import app
-from app import socketio
 import os
 
 if not os.path.exists("backend/instance/site.db"):
@@ -8,5 +7,5 @@ if not os.path.exists("backend/instance/site.db"):
     with app.app_context():
         db.create_all()
 
-if __name__ == '__main__':
-    socketio.run(app=app)
+# if __name__ == '__main__':
+#     socketio.run(app=app)
