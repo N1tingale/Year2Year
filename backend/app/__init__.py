c4e622e83f29669793c4bba78d4498b22cbd3a6e
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-04-14 22:54:05 +0100

    change socketio in app

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_socketio import SocketIO
+# from flask_socketio import SocketIO
 from flask_mail import Mail, Message
 from asgiref.wsgi import WsgiToAsgi
+import socketio
 
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='asgi')
+socketio = socketio.AsyncServer()
+
+# socketio = SocketIO(app, cors_allowed_origins="*", async_mode='asgi')
 app.config.from_object('config.Config')
+app = socketio.WSGIApp(socketio, app)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-asgi_app = WsgiToAsgi(app)
+# asgi_app = WsgiToAsgi(app)
 
 mail = Mail(app)
 db = SQLAlchemy(app)
