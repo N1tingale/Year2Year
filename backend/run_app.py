d5fe2cd903e1d907f7b944fa8e74d4f1cf43d79c
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2024-04-14 19:49:59 +0100

    more changes for prod

--- backend/run_app.py
+++ backend/run_app.py
@@ -8,9 +8,4 @@ if not os.path.exists("backend/instance/site.db"):
         db.create_all()
 
 if __name__ == '__main__':
-    from werkzeug.serving import run_simple
-
-    HOST = '0.0.0.0'
-    PORT = 8000
-
-    run_simple(HOST, PORT, app)
+    app.run()
